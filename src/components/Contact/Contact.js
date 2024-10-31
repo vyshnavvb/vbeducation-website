479f61abb25c55aa2ad71747ccb0615659a73779
vyshnavvb <vyshnavvb60@gmail.com>
2024-10-31 23:42:16 +0530

    new files added

---
+++ src/components/Contact/Contact.js
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import "./Contact.css"
+
+const ContactPage = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(`Name: ${name}, Email: ${email}`);
+  };
+
+  return (
+    <div className="contact-body">
+      <h2 className="c-heading">Contact Us</h2>
+      <form className="cform" onSubmit={handleSubmit}>
+        <div className="cdiv">
+          <label className="clabel">Name:</label>
+          <input
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+        <div>
+          <label>Email:</label>
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <button  type="submit" className="cbutton">Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default ContactPage;
---
+++ src/components/Terms/Terms.js
@@ -0,0 +1,40 @@
+import React from "react";
+import "./Terms.css";
+
+const TermsAndConditions = () => {
+  return (
+    <div className="terms-conditions">
+      <h2 className="th2">Terms and Conditions</h2>
+      <p className="tp">
+        Welcome to our website. By accessing and using this website, you agree
+        to comply with the following terms and conditions:
+      </p>
+      <h3 className="th3">1. Acceptance of Terms</h3>
+      <p className="tp">
+        By using this site, you agree to be bound by these terms and conditions
+        of use.
+      </p>
+      <h3 className="th3">2. Changes to Terms</h3>
+      <p className="tp">
+        We reserve the right to modify these terms at any time. Any changes will
+        be effective immediately upon posting.
+      </p>
+      <h3 className="th3">3. Use of Site</h3>
+      <p className="tp">
+        You agree to use this site for lawful purposes only and not for any
+        prohibited activities.
+      </p>
+      <h3 className="th3">4. Intellectual Property</h3>
+      <p className="tp">
+        All content on this site is the property of our organization and
+        protected by copyright laws.
+      </p>
+      <h3 className="th3">5. Limitation of Liability</h3>
+      <p className="tp">
+        We are not liable for any damages arising from the use of this site.
+      </p>
+    </div>
+  );
+};
+
+export default TermsAndConditions;
